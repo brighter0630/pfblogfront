1d8976c784c32cc6e76414053dd5998bc037e1f0
Junghoon Lee <brighter87@gmail.com>
2023-10-14 23:07:56 +0900

    a

--- components/MDXcomponents/PriceChartMDX.js
+++ components/MDXcomponents/PriceChartMDX.js
@@ -1,8 +1,11 @@
+"use client";
+
 import PriceChart from "../../components/PriceChart";
 import getPriceHistory from "../../libs/getPriceHistory.js";
 
 async function PriceChartTemplete({ params }) {
   const { ticker, charttype } = params;
+  console.log(ticker, charttype);
   const { symbol, historical } = await getPriceHistory(ticker);
 
   return (
---
+++ posts/Test2.jsx
@@ -0,0 +1,3 @@
+export default function Test2() {
+  return <div>안녕하세요 머저리드랑?</div>;
+}
