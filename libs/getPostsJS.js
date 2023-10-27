6495147853afffd325d44619b2b2b839b6def7ea
brighter0630 <brighter87@gmail.com>
2023-10-27 17:30:36 +0900

    a

--- libs/getPostsJS.js
+++ libs/getPostsJS.js
@@ -4,32 +4,31 @@ import path from "path";
 const rootDirectory = path.join(process.cwd(), "app", "post");
 
 export const getPostBySlug = (slug) => {
-	const realSlug = decodeURIComponent(slug);
-	const { metaData } = require(`../app/post/${realSlug}/page`);
+  const realSlug = decodeURIComponent(slug);
+  const { blogMeta } = require(`../app/post/${realSlug}/page`);
 
-	return { metaData };
+  return { blogMeta };
 };
 
 export const getAllPostsMeta = async () => {
-	try {
-		const files = fs
-			.readdirSync(rootDirectory)
-			.filter((file) => !file.includes("layout.js"));
+  try {
+    const files = fs
+      .readdirSync(rootDirectory)
+      .filter((file) => !file.includes("layout.js"));
 
-		let posts = [];
-		for (const file of files) {
-			try {
-				const { metaData } = getPostBySlug(file);
-				if (metaData.published) {
-					posts.push({ ...metaData, slug: file });
-				}
-			} catch (error) {
-				console.error("Error in Pushing MetaData In", error);
-			}
-		}
-		return posts;
-	}
-	catch (error) {
-		console.error("Failed Fetching Post Data", error);
-	}
+    let posts = [];
+    for (const file of files) {
+      try {
+        const { blogMeta } = getPostBySlug(file);
+        if (blogMeta.published) {
+          posts.push({ ...blogMeta, slug: file });
+        }
+      } catch (error) {
+        console.error("Error in Pushing blogMeta In", error);
+      }
+    }
+    return posts;
+  } catch (error) {
+    console.error("Failed Fetching Post Data", error);
+  }
 };
