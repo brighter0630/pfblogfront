d72a1307bc8ebbec55e5ad51a06599c96a37404d
Junghoon Lee <brighter87@gmail.com>
2023-11-06 17:03:01 +0900

    a

--- components/news/ArticleImage.js
+++ components/news/ArticleImage.js
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 
-export default function({ newsUrl, width, height }) {
+export default function ArticleImage({ newsUrl, width, height }) {
 	return (
 		<Image loader={() => `${newsUrl}`} src="noimage.png" alt="No Image" width={width} height={0} priority={false} style={{height: 'auto'}} />
 	)
